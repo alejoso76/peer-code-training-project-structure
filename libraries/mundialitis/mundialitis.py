367f44ae4ac9a5fb55cd1e2a5b4a0570f41a8f4c
alejoso76 <alejandroosoriotrujillo@gmail.com>
2022-02-18 18:23:32 -0500

    game played

--- libraries/mundialitis/mundialitis.py
+++ libraries/mundialitis/mundialitis.py
@@ -1,5 +1,6 @@
-from libraries.common import act_on_element, capture_page_screenshot
+from libraries.common import act_on_element, capture_page_screenshot, log_message
 from config import OUTPUT_FOLDER
+import random
 
 
 class Mundialitis:
@@ -16,11 +17,12 @@ class Mundialitis:
 
         # Extras
         self.mundialitis_login = credentials['login']
-        #* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        #* USE ENGLISH KEAYBOARD WHEN TESTING
+        # * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # * USE ENGLISH KEAYBOARD WHEN TESTING
         # super + space -> en
-        #* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         self.mundialitis_password = credentials['password']
+        self.lobby = ''
         # self.mundialitis_login = credentials['login']
         # self.mundialitis_password = 'P33RC@DETR@INING'
 
@@ -32,6 +34,7 @@ class Mundialitis:
             self.input_credentials()
             self.submit_form()
         except Exception as e:
+            print(str(e))
             capture_page_screenshot(
                 OUTPUT_FOLDER, 'Exception_Mundialitis_Login')
             raise Exception('Login to Mundialitis failed')
@@ -42,10 +45,12 @@ class Mundialitis:
         # For basic stuff, we can use implicit wait
         # from selenium
         # xpath of element to click
-        #ctrl + f to activate xpath searcher
+        # ctrl + f to activate xpath searcher
         self.browser.click_element('//a[text()="LOGIN"]')
-        self.browser.input_text_when_element_is_visible('//input[@id="logusername"]', self.mundialitis_login)
-        self.browser.input_text_when_element_is_visible('//input[@id="logpassword"]', self.mundialitis_password)
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="logusername"]', self.mundialitis_login)
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="logpassword"]', self.mundialitis_password)
         return
 
     def submit_form(self):
@@ -55,3 +60,102 @@ class Mundialitis:
         # check element of page after login
         act_on_element('//div[@id="main"]', 'find_element')
         return
+
+    def create_lobby(self):
+        """Function that creates a new lobby for trivia
+        """
+        new_lobby = 'RPA Lobby 12'
+        act_on_element('//a[@href="/trivialobbies"]', 'click_element')
+        act_on_element('//button[text()="CREAR LOBBY"]', 'click_element')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="lobbyname"]', 'Lobby lo max')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="lobbyname"]', new_lobby)
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="lobbypassword"]', new_lobby)
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="lobbymoney"]', '500')
+        act_on_element(
+            '//button[@name="createlobby" and @type="submit"]', 'click_element')
+        self.lobby = new_lobby
+
+    def register_new_user(self):
+        """Function that registers a new user
+        """
+        self.browser.go_to(self.mundialitis_url)
+
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="rusername"]', 'testusercito1234')
+        password = 'test_password'
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="rpassword"]', password)
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="rpassword2"]', password)
+        act_on_element(
+            '//button[@name="register" and @type="submit"]', 'click_element')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="rfirstname"]', 'TEST')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="rlastname"]', 'TEST')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="remail"]', 'test@testmail.com')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="raddress"]', 'Test address')
+        act_on_element(
+            '//select[@id="rcountry"]', 'click_element')
+        act_on_element(
+            '//select[@id="rcountry"]/option[@value="Colombia"]', 'click_element')
+        self.browser.input_text_when_element_is_visible(
+            '//input[@id="rmoney"]', "100")
+        act_on_element(
+            '//button[@name="cmpregister" and @type="submit"]', 'click_element')
+        act_on_element(
+            '//div[@id="main"]', 'find_element')
+
+    def join_lobby(self, mode: str):
+        """Function that joins an specified lobby
+        """
+        act_on_element('//a[@href="/trivialobbies"]', 'click_element')
+        if mode == 'creator':
+            act_on_element(
+                F'//li[@class="list-group-item"]/a[text()="{self.lobby + " - Unido"}"]', 'click_element')
+        else:
+            act_on_element(
+                F'//li[@class="list-group-item"]/a[text()="{self.lobby}"]', 'click_element')
+            self.browser.input_text_when_element_is_visible(
+                F'//input[@id="lobbypassword"]', self.lobby)
+            act_on_element('//button[@name="accesslobby"]', 'click_element')
+            act_on_element('//button[@id="joinlobby"]', 'click_element')
+
+    def start_game(self):
+        """Function that starts the game with the user that created the lobby
+        """
+        act_on_element('//button[text()="INICIAR JUEGO"]', 'click_element')
+        act_on_element('//button[text()="Difícil"]', 'click_element')
+
+    def play_game(self):
+        """Function that plays the game
+        """
+        questions_remaining = True
+        while questions_remaining:
+            try:
+                act_on_element('//div[@class="list-group"]', 'find_element')
+            except:
+                questions_remaining = False
+            # Only runs if the except wasnt raised
+            else:
+                answer_elements = act_on_element(
+                    '//button[contains(@id, "optn") and position() > 1]', 'find_elements')
+                selected_answer_index = random.randint(
+                    0, len(answer_elements) - 1)
+                act_on_element(
+                    answer_elements[selected_answer_index], 'click_element')
+                act_on_element(
+                    '//a[@class="btn btn-lg mt-4 btn-outline-light" and text()="Siguiente"]', 'click_element')
+
+        results = act_on_element(
+            '//li[@class="list-group-item"]', 'find_element').text.split(': ')
+
+        log_message(F"Username: {results[0]}")
+        log_message(F"Score: {results[1]}")
+        capture_page_screenshot(OUTPUT_FOLDER, 'Mundialitis_Trivia_Results')
--- libraries/process.py
+++ libraries/process.py
@@ -18,22 +18,54 @@ class Process:
 
             # For pdf tab and prompt to save or open file
             'plugins.always_opens_pdf_externally': True,
-            'download.prompt_for_download': False
+            'download.prompt_for_download': False,
+            # "chrome.prefs": {
+            #     'Chromedriver.required_version': '97.0.4692.71'
+            # }
         }
         # Blank browser
-        browser.open_available_browser(preferences=prefs)
+        # chrome esta molestando
+        browser.open_available_browser(
+            preferences=prefs, browser_selection=['firefox'])
+
+        # browser.open_available_browser(
+        #     preferences=prefs)
         browser.set_window_size(1920, 1080)
         browser.maximize_browser_window()
         mundialitis = Mundialitis(browser, credentials['Mundialitis'])
         mundialitis.login()
+        self.mundialitis = mundialitis
 
     def start(self):
-        log_message("Macro Step 1")
-        log_message("Macro Step 2")
-        log_message("Macro Step 3")
+        log_message("Start - Create New Lobby")
+        self.mundialitis.create_lobby()
+        log_message("Finish - Create New Lobby")
+
+        log_message("Start - Register New User")
+        self.mundialitis.register_new_user()
+        log_message("Finish - Register New User")
+
+        log_message("Start - Join Lobby")
+        self.mundialitis.join_lobby('user')
+        log_message("Finish - Join Lobby")
+
+        log_message("Start - Login with first user")
+        self.mundialitis.login()
+        log_message("Finish - Login with first user")
+
+        log_message("Start - Join Lobby with first user")
+        self.mundialitis.join_lobby('creator')
+        log_message("Finish - Join Lobby with first user")
+
+        log_message("Start - Start game with first user")
+        self.mundialitis.start_game()
+        log_message("Finish - Start game with first user")
+
+        log_message("Start - Play game with first user")
+        self.mundialitis.play_game()
+        log_message("Finish - Play game with first user")
 
     def finish(self):
-        capture_page_screenshot(OUTPUT_FOLDER, 'Mundialitis_Login')
         log_message("DW Process Finished")
         # Good practice: close browser manually
         browser.close_browser()
